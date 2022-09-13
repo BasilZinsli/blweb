70849d5f281d3a18a6a09412a6d1b03429d3ab61
SeluxXs <basil.zinsli@gmail.com>
2022-09-13 23:21:11 +0200

    NAvTop

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Nav.css';
 import Logo from  './logo.png'; 
 import {Link} from 'react-scroll';
-import {FaBars, FaTimes } from 'react-icons/fa'
+import {FaBars, FaTimes, FaYoutube } from 'react-icons/fa'
 import {useState} from 'react';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebook, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons"
 
 function Nav(){
     const [mobileList, setMobileList] = useState(false); 
@@ -21,6 +22,19 @@ function Nav(){
                 
                 <img className='logo' src={Logo} alt='logo'></img>
                 
+            <div className='socLinks1'>
+                <a href='https://www.instagram.com/birdland_band/?hl=de' className='Insta' style={{ border: "none"}}>
+                    <FontAwesomeIcon icon={faInstagram} size="xm" color='white' />   
+                </a>
+
+                <a href='https://www.youtube.com/channel/UCAg9boOxNWyH3bjmNx7_0Sw' className='Youtube' style={{ border: "none"}}>
+                    <FontAwesomeIcon icon={faYoutube} size="xm" color='White'/>   
+                </a>
+
+                <a href='https://www.facebook.com/people/Birdland-Eventband/100075762588480/' className='Facebook' style={{ border: "none"}}>
+                    <FontAwesomeIcon icon={faFacebook} size="xm" color='White' /> 
+                </a>
+            </div>  
                 <button className='hamburger' style={{ border: "none"}} onClick={toggleList}>
                     {mobileList ? (<FaTimes size={30}  style={{ color: '#ffffff'}} />): (<FaBars size={30} style={{ color: '#ffffff' }} />)}    
                 </button>
@@ -43,6 +57,7 @@ function Nav(){
             </div>
             
             {mobileList?(
+                
             <ul className='listMobile'>
                <li> 
                     <Link activeClass="active" onClick={toggleList} to="Home" spy={true} smooth={true} offset={-70} duration={500}><span>Home</span><i></i></Link> 
